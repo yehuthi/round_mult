d65cff54781294e746a4ddef965ff7fbc165bb51
Yehuthith <yehuthith@gmail.com>
2022-08-12 12:39:41 +0300

    Implement rounding up

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::ops::{BitAnd, Not, Sub};
+use std::ops::{Add, BitAnd, Not, Sub};
 
 /// The trait for numbers in this library.
 ///
@@ -47,4 +47,19 @@ impl<T: num_traits::One + Sub<Output = Self> + Not<Output = Self> + BitAnd<Outpu
 #[inline(always)]
 pub fn down<N: Num>(value: N, mult: N) -> N {
 	value & !(mult - N::one())
+}
+
+/// Rounds the `value` up to the nearest multiplier of `mult`.
+///
+/// # Examples
+/// ```
+/// assert_eq!(
+///     round_mult::up(101, 10),
+///     110
+/// );
+/// ```
+#[inline(always)]
+pub fn up<N: Num + Add<Output = N> + Copy>(value: N, mult: N) -> N {
+	// TODO: specialized implementation
+	down(value, mult) + mult
 }

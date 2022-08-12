6aac182a57732c3df74db934667ede4f34410dc3
Yehuthith <yehuthith@gmail.com>
2022-08-12 10:21:09 +0300

    Implement round down

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use std::ops::{BitAnd, Not, Sub};
+
+pub trait Num: Sub<Output = Self> + Not<Output = Self> + BitAnd<Output = Self> + Sized {
+	fn one() -> Self;
+}
+
+impl<
+		T: num_traits::One + Sub<Output = Self> + Not<Output = Self> + BitAnd<Output = Self> + Sized,
+	> Num for T
+{
+	fn one() -> Self {
+		<T as num_traits::One>::one()
+	}
+}
+
+/// Rounds the `value` down to the nearest multiplier of `mult`.
+///
+/// # Examples
+/// ```
+/// assert_eq!(
+///     round_mult::down(109, 10),
+///     100
+/// );
+/// ```
+pub fn down<N: Num>(value: N, mult: N) -> N {
+	value & !(mult - N::one())
+}

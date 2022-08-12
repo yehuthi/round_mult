8f967c643a3f25ab15dfa995ad7dc6c52eb429a6
Yehuthith <yehuthith@gmail.com>
2022-08-12 13:01:51 +0300

    Add rounding up benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -22,5 +22,38 @@ pub fn bench_round_down(c: &mut Criterion) {
 	g.finish();
 }
 
+pub fn bench_round_up(c: &mut Criterion) {
+	fn naive_round_up_1(value: usize, mult: usize) -> usize {
+		(value / mult * mult) + mult
+	}
+
+	fn naive_round_up_2(value: usize, mult: usize) -> usize {
+		((value + mult - 1) / mult) * mult
+	}
+
+	let mut g = c.benchmark_group("Round Up");
+
+	g.bench_function("round_mult", |b| {
+		b.iter(|| {
+			black_box(round_mult::up::<usize>(black_box(109), 10));
+		});
+	});
+
+	g.bench_function("Naive #1", |b| {
+		b.iter(|| {
+			black_box(naive_round_up_1(black_box(109), 10));
+		});
+	});
+
+	g.bench_function("Naive #2", |b| {
+		b.iter(|| {
+			black_box(naive_round_up_2(black_box(109), 10));
+		});
+	});
+
+	g.finish();
+}
+
+criterion_group!(group_round_up, bench_round_up);
 criterion_group!(group_round_down, bench_round_down);
-criterion_main!(group_round_down);
+criterion_main!(group_round_down, group_round_up);

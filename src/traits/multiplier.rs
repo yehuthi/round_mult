cbae96b786b2afbfca25f584fac744aaea6c4f5b
Yehuthith <yehuthith@gmail.com>
2022-08-19 02:15:56 +0300

    Improve documentation

--- src/traits/multiplier.rs
+++ src/traits/multiplier.rs
@@ -1,3 +1,5 @@
+use std::num::{NonZeroU128, NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU8, NonZeroUsize};
+
 use crate::{private::Number, NonZeroPow2};
 
 use super::NonZeroable;
@@ -20,9 +22,9 @@ pub(crate) mod private {
 pub trait Multiplier: public::Multiplier + private::Multiplier {}
 
 macro_rules! impl_unsigned_number {
-	($($ty:ty),* $(,)?) => {
+	($($ty:ty : $nz:ty),* $(,)?) => {
 		$(
-			impl public::Multiplier for <$ty as crate::traits::nonzero::public::NonZeroable>::NonZeroType {
+			impl public::Multiplier for $nz {
 				type Number = $ty;
 			}
 
@@ -52,12 +54,19 @@ macro_rules! impl_unsigned_number {
 				}
 			}
 
-			impl Multiplier for <$ty as crate::traits::nonzero::public::NonZeroable>::NonZeroType {}
+			impl Multiplier for $nz {}
 		)*
 	};
 }
 
-impl_unsigned_number!(u8, u16, u32, u64, u128, usize);
+impl_unsigned_number!(
+	u8: NonZeroU8,
+	u16: NonZeroU16,
+	u32: NonZeroU32,
+	u64: NonZeroU64,
+	u128: NonZeroU128,
+	usize: NonZeroUsize
+);
 
 impl<N: NonZeroable> public::Multiplier for NonZeroPow2<N> {
 	type Number = N;

25cf1ca7bcb885975e5b746567055a0d7b223737
yehuthi <yehuthith@gmail.com>
2025-03-25 18:24:36 +0200

    improve benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -24,5 +24,23 @@ pub fn bench_round_down(c: &mut Criterion) {
 	g.finish();
 }
 
+pub fn bench_round_up(c: &mut Criterion) {
+	let mult32 = NonZeroPow2::<usize>::v32();
+	let mut g = c.benchmark_group("Round Up");
+
+	g.bench_function("lib round up NZP", |b| {
+		b.iter(|| {
+			black_box(round_mult::up(black_box(100usize), black_box(mult32)))
+		});
+	});
+
+	g.bench_function("std round up NZP", |b| {
+		b.iter(|| {
+			black_box(black_box(100usize).next_multiple_of(32))
+		});
+	});
+}
+
 criterion_group!(group_round_down, bench_round_down);
-criterion_main!(group_round_down);
+criterion_group!(group_round_up, bench_round_up);
+criterion_main!(group_round_down, group_round_up);

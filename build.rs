2193645ca4e6e86c9120b0703020008f125e7dc3
yehuthi <yehuthith@gmail.com>
2025-03-25 18:15:21 +0200

    fix nighty build check

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 use rustc_version::Channel;
 
 fn main() {
+	println!("cargo::rustc-check-cfg=cfg(nightly)");
     if rustc_version::version_meta().unwrap().channel == Channel::Nightly {
         println!("cargo:rustc-cfg=nightly");
     }
